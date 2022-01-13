a5b353be0609a309f42ad1803f88c1eded5754d9
rbwo <rbwo552@naver.com>
2022-01-13 23:59:34 +0900

    2022_01_13

--- ch.js
+++ ch.js
@@ -134,3 +134,22 @@ function foo7() {
 
 const baz = foo7();
 baz(); // 1
+
+
+var value = 1;
+
+var obj = {
+  value: 100,
+  foo: function() {
+    setTimeout(function() {
+      console.log("callback's this: ",  this);  // ?
+      console.log("callback's this.value: ",  this.value);  // ?
+      function bar() {
+        console.log("bar's this: ", this);  // ?
+      }
+      bar();
+    }, 1000);
+  }
+};
+
+obj.foo();
